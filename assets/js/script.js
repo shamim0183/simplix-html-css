f40102602f60b636f1e8300ef634254bb6095223
shamim_personal <shaamim.hossaain@gmail.com>
2023-03-14 12:59:31 +0600

    simplix with html css added

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+    // Set div display to none
+    $(".close_btn").click(function () {
+        // $(".sidebar").css("left", "-400px");
+        $(".sidebar").removeClass("toggle_menu");
+    });
+
+    // Set div display to block
+    $(".toggle").click(function () {
+        // $(".sidebar").css("left", "0px");
+        $(".sidebar").addClass("toggle_menu");
+    });
+    $("#form-date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+        mondayFirst: true,
+    });
+    $("#to-date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+
+    // Employee table
+    $(".employee_wrapper button.nav-link").on("click", function () {
+        let id = $(this).attr('id');
+        if (id == 'pills-contact-tab') {
+            $("#employee-pagination").addClass("d-none");
+
+        } else {
+
+            $("#employee-pagination").removeClass("d-none");
+        }
+    });
+
+    $("#time").pDatePicker({
+        showDate: new Date(2023, 1), // Feb 2023
+    });
+
+    //
+    $("#Nationality").select2({
+        placeholder: "Nationality",
+        allowClear: true
+    });
+    $("#Month").select2({
+        placeholder: "Weekly",
+        allowClear: true
+    });
+    $("#code").select2({
+        placeholder: "R",
+        allowClear: true
+    });
+    $("#Mobile-Number").intlTelInput({
+        customPlaceholder: null,
+        allowDropdown: true,
+        separateDialCode: true,
+        initialCountry: "ca",
+
+    });
+    $("#Phone-Number").intlTelInput({
+        customPlaceholder: null,
+        allowDropdown: true,
+        separateDialCode: true,
+        initialCountry: "ca",
+    });
+    $("#Hire-Date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+    $("#hire_date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+    $("#birth_date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+    $("#one-date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+    $("#two-date").pDatePicker({
+        selected: new Date(2023, 1, 20), // Feb 20, 2023
+
+    });
+
+    //user
+
+
+
+});
